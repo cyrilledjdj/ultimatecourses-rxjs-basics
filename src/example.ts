7ff76756a5f40d9f67a386653754827c90247052
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-08-25 21:17:42 -0400

    manage observable subscriptions with unsubscribe

--- src/example.ts
+++ src/example.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
+import { Observer } from 'rxjs/internal/types';
 
-const observer = {
+const observer: Observer<any> = {
     next: (value: any) => console.log('next', value),
     error: (error: any) => console.log('error', error),
     complete: () => console.log('complete!')
@@ -10,8 +11,7 @@ const observable = new Observable(subscriber => {
     let count = 0;
     const id = setInterval(() => {
         subscriber.next(count++);
-        subscriber.complete();
-        console.log('stepped');
+        // subscriber.complete();
     }, 1000);
 
     return () => {
@@ -21,5 +21,11 @@ const observable = new Observable(subscriber => {
 });
 
 console.log('before')
-observable.subscribe(observer);
+const subscription = observable.subscribe(observer);
+const subscription2 = observable.subscribe(observer);
+subscription.add(observable.subscribe(observer));
 console.log('after')
+setTimeout(() => {
+    subscription.unsubscribe();
+    subscription2.unsubscribe();
+}, 3000)
