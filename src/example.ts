533e7c6ba0adf68acaeaf280ebe6cbeed81e6991
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-08-25 21:00:10 -0400

    understanding observers, partial observers and subscribers

--- src/example.ts
+++ src/example.ts
@@ -1,10 +1,10 @@
 import { Observable } from 'rxjs';
 
-const observer = {
-    next: (value: any) => console.log('next', value),
-    error: (error: any) => console.log('error', error),
-    complete: () => console.log('complete!')
-}
+// const observer = {
+//     next: (value: any) => console.log('next', value),
+//     error: (error: any) => console.log('error', error),
+//     complete: () => console.log('complete!')
+// }
 
 const observable = new Observable(subscriber => {
     subscriber.next('hello');
@@ -13,4 +13,9 @@ const observable = new Observable(subscriber => {
     subscriber.next('No Called')
 });
 
-observable.subscribe(observer);
+observable.subscribe(
+    (value: any) => console.log('next', value),
+    // (error: any) => console.log('error', error),
+    null,
+    () => console.log('complete!')
+);
