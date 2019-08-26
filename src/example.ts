cc04f48f9539a1a280982fab83f7b0a245022fdf
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-08-25 21:06:49 -0400

    deliver values asynchronously with observables

--- src/example.ts
+++ src/example.ts
@@ -1,21 +1,25 @@
 import { Observable } from 'rxjs';
 
-// const observer = {
-//     next: (value: any) => console.log('next', value),
-//     error: (error: any) => console.log('error', error),
-//     complete: () => console.log('complete!')
-// }
+const observer = {
+    next: (value: any) => console.log('next', value),
+    error: (error: any) => console.log('error', error),
+    complete: () => console.log('complete!')
+}
 
 const observable = new Observable(subscriber => {
-    subscriber.next('hello');
-    subscriber.next('World');
-    subscriber.complete();
-    subscriber.next('No Called')
+    let count = 0;
+    const id = setInterval(() => {
+        subscriber.next(count++);
+        subscriber.complete();
+        console.log('stepped');
+    }, 1000);
+
+    return () => {
+        console.log('called');
+        clearInterval(id);
+    }
 });
 
-observable.subscribe(
-    (value: any) => console.log('next', value),
-    // (error: any) => console.log('error', error),
-    null,
-    () => console.log('complete!')
-);
+console.log('before')
+observable.subscribe(observer);
+console.log('after')
