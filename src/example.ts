e8ee437e605d2ccba8db2da3de78da8f2d0054ee
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-08-26 20:52:10 -0400

    create observables from dom events using fromevent

--- src/example.ts
+++ src/example.ts
@@ -1,31 +1,44 @@
-import { Observable } from 'rxjs';
-import { Observer } from 'rxjs/internal/types';
+import { fromEvent } from 'rxjs';
 
-const observer: Observer<any> = {
-    next: (value: any) => console.log('next', value),
-    error: (error: any) => console.log('error', error),
-    complete: () => console.log('complete!')
+// const observer: Observer<any> = {
+//     next: (value: any) => console.log('next', value),
+//     error: (error: any) => console.log('error', error),
+//     complete: () => console.log('complete!')
+// }
+
+// const observable$ = new Observable(subscriber => {
+//     let count = 0;
+//     const id = setInterval(() => {
+//         subscriber.next(count++);
+//         // subscriber.complete();
+//     }, 1000);
+
+//     return () => {
+//         console.log('called');
+//         clearInterval(id);
+//     }
+// });
+
+// console.log('before')
+// const subscription = observable$.subscribe(observer);
+// const subscription2 = observable$.subscribe(observer);
+// subscription.add(observable$.subscribe(observer));
+// console.log('after')
+// setTimeout(() => {
+//     subscription.unsubscribe();
+//     subscription2.unsubscribe();
+// }, 3000)
+
+const source$ = fromEvent(document, 'click');
+const observer = {
+    next: value => console.log('next', value),
+    error: err => console.log('error', err),
+    complete: () => console.log('complete')
 }
+const sub1 = source$.subscribe(observer);
+sub1.add(source$.subscribe(observer));
 
-const observable = new Observable(subscriber => {
-    let count = 0;
-    const id = setInterval(() => {
-        subscriber.next(count++);
-        // subscriber.complete();
-    }, 1000);
-
-    return () => {
-        console.log('called');
-        clearInterval(id);
-    }
-});
-
-console.log('before')
-const subscription = observable.subscribe(observer);
-const subscription2 = observable.subscribe(observer);
-subscription.add(observable.subscribe(observer));
-console.log('after')
 setTimeout(() => {
-    subscription.unsubscribe();
-    subscription2.unsubscribe();
+    console.log('unsubscribing');
+    sub1.unsubscribe();
 }, 3000)
